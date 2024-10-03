8a27f1eedd7a88947f33a35d08e3a09a590fcaa8
Shivam Raikar <90413359+Proshivam23@users.noreply.github.com>
2024-10-03 19:38:16 +0530

    feat: integrate remote PostgreSQL database with NeonDB

--- backend/data-source.js
+++ backend/data-source.js
@@ -1,18 +1,18 @@
 const { DataSource } = require('typeorm');
+require('dotenv').config();
 const RegisteredUserDetails = require("./src/entity/eventRegisteredUserDetails");  
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",    // Replace with your PostgreSQL host
-  port: 5432,           // Default PostgreSQL port
-  username: "postgres", // Replace with your PostgreSQL username
-  password: "shivam", // Replace with your PostgreSQL password
-  database: "college", // Replace with your PostgreSQL database name
+  url: process.env.DATABASE_URL,
   synchronize: true,    // Set to false in production
   logging: false,
   entities: [RegisteredUserDetails],  // Path to your entity files
   migrations: [],
   subscribers: [],
+  ssl: {
+    rejectUnauthorized: false, // This might be required for NeonDB
+  }
 });
 
 module.exports = AppDataSource;
